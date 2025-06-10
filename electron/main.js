a33ad79c95d64574602664cf753f0fa7aaa299a4
Pranav <coolpranav.r@gmail.com>
2025-06-09 19:32:27 -0500

    Add customized Login Card

--- electron/main.js
+++ electron/main.js
@@ -1,6 +1,6 @@
-import { app, BrowserWindow } from 'electron';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import { app, BrowserWindow } from "electron";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,16 +9,18 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
     height: 700,
+    icon: "../public/logo.png",
     webPreferences: {
-      preload: join(__dirname, 'preload.js') 
+      preload: join(__dirname, "preload.js"),
     },
   });
 
-  win.loadURL('http://localhost:5173');
+  win.loadURL("http://localhost:5173");
+  win.setMenuBarVisibility(false);
 }
 
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
---
+++ src/screens/LoginScreen/LoginScreen.jsx
@@ -0,0 +1,9 @@
+import "./LoginScreen.css";
+import "../../global.css";
+import LoginForm from "../../components/LoginForm/LoginForm.jsx";
+
+function LoginScreen() {
+  return <LoginForm />;
+}
+
+export default LoginScreen;
